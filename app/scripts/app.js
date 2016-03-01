310080c8352b846538f66d66d6250972fb40c846
Riera Valenciano <Laura.RieraValenciano.external@ts.fujitsu.com>
2016-03-01 18:07:16 +0100

    init install configuration satellizer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,21 +15,95 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'satellizer'
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'scripts/main/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: 'scripts/about/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
+      })
+      .when('/login', {
+        templateUrl: 'scripts/login/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .when('/signup', {
+        controller: 'SignupCtrl',
+        controllerAs: 'signup'
+      })
+      .when('/logout', {
+        controller: 'LogoutCtrl',
+        controllerAs: 'logout'
       })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .config(function($authProvider) {
+
+    $authProvider.facebook({
+      clientId: '223985831285204',
+      authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+      redirectUri: window.location.origin + '/',
+      popupOptions: { width: 580, height: 400 }
+    });
+
+    $authProvider.google({
+      clientId: 'Google Client ID'
+    });
+
+    $authProvider.github({
+      clientId: 'GitHub Client ID'
+    });
+
+    $authProvider.linkedin({
+      clientId: 'LinkedIn Client ID'
+    });
+
+    $authProvider.instagram({
+      clientId: 'Instagram Client ID'
+    });
+
+    $authProvider.yahoo({
+      clientId: 'Yahoo Client ID / Consumer Key'
+    });
+
+    $authProvider.live({
+      clientId: '000000004C18335A'
+    });
+
+    $authProvider.twitch({
+      clientId: 'Twitch Client ID'
+    });
+
+    $authProvider.bitbucket({
+      clientId: 'Bitbucket Client ID'
+    });
+
+    // No additional setup required for Twitter
+
+    // Twitter
+    $authProvider.twitter({
+      authorizationEndpoint: 'https://api.twitter.com/oauth/authenticate',
+      redirectUri: window.location.origin,
+      type: '1.0',
+      popupOptions: { width: 495, height: 645 }
+    });
+
+    $authProvider.oauth2({
+      name: 'Login',
+      url: '/#/login',
+      clientId: 'Foursquare Client ID',
+      redirectUri: window.location.origin,
+      authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate',
+    });
+
   });
---
+++ app/scripts/login/login.js
@@ -0,0 +1,61 @@
+(function() {
+  'use strict';
+
+
+  /**
+   * @ngdoc function
+   * @name satellizerApp.controller:LoginCtrl
+   * @description
+   * # LoginCtrl
+   * Controller of the satellizerApp
+   */
+  angular
+    .module('satellizerApp')
+    .controller('LoginCtrl', LoginCtrl);
+
+  LoginCtrl.$inject = ['$scope', '$auth'];
+
+  function LoginCtrl($scope, $auth) {
+    var vm = this;
+    var user = {
+      email: vm.email,
+      password: vm.password
+    };
+
+    vm.authenticate = function(provider) {
+      $auth.setStorageType('sessionStorage');
+      console.log($auth);
+      alert("vm authenticate");
+      $auth.authenticate(provider);
+      $auth.login()
+    };
+
+    $auth.authenticate('google')
+      .then(function(response) {
+         console.log(response);
+        console.log('google');
+      })
+      .catch(function(response) {
+        console.log(response);
+      });
+
+    $auth.authenticate('twitter')
+      .then(function(response) {
+        console.log(response);
+        console.log('twitter');
+      })
+      .catch(function(response) {
+        console.log(response);
+      });
+
+    $auth.authenticate('facebook')
+      .then(function(response) {
+        console.log('facebook');
+        console.log(response);
+      })
+      .catch(function(response) {
+        console.log(response);
+      });
+
+  }
+})();
