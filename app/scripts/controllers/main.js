5d65c63948abb30c0e75f9ad9052cc6296327f9d
Riera Valenciano <Laura.RieraValenciano.external@ts.fujitsu.com>
2016-03-01 11:32:27 +0100

    Deploy with satellizer

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name satellizerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the satellizerApp
+ */
+angular.module('satellizerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
